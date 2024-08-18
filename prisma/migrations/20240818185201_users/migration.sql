5c01c7c629aedfb00198329e4b133f02bcd1b8dd
nazmul53p <nazmul2018s@gmail.com>
2024-08-19 00:52:56 +0600

    doc

--- prisma/migrations/20240818185201_users/migration.sql
+++ prisma/migrations/20240818185201_users/migration.sql
@@ -4,7 +4,7 @@ CREATE TABLE "User" (
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "updatedAt" TIMESTAMP(3),
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
